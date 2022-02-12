914d4a2ef2af557d4414cb1a6dce2c0d5d958978
mostafasivision <mostafa.ali@si-vision.com>
2022-02-12 17:28:21 +0200

    3 files modified

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,13 +1,13 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { Product } from '../products';
+import { Product } from '../products'; // just to understant the interface Product
 @Component({
   selector: 'app-product-alerts',
   templateUrl: './product-alerts.component.html',
   styleUrls: ['./product-alerts.component.css'],
 })
 export class ProductAlertsComponent {
-  @Input() product!: Product;
-  @Output() notify = new EventEmitter();
+  @Input() product!: Product; // import product data from  product list (parent) to product-alerts (child)
+  @Output() notifyParent = new EventEmitter();
   constructor() {}
 
   ngOnInit(): void {}
